cc65b22e9064f5729bee07d78f1cd16eb6bfd7a1
CamilaAlbieri <camilamattos.mila@gmail.com>
2024-08-08 00:11:23 -0300

    Criação App com react native

---
+++ app/index.tsx
@@ -0,0 +1,20 @@
+import {View, Text, StyleSheet} from 'react-native'
+
+  export default function App(){
+    return(
+      <View style={styles.container}>
+        <Text>App</Text>
+      </View>
+    )
+  }
+
+
+  const styles = StyleSheet.create({
+    container:{
+      flex:1,
+      backgroundColor: "#9731eb",
+      justifyContent: "center",
+      alignItems: "center"
+    }
+
+  })
